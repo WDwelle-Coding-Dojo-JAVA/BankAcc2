a43a70128699682bbad0ec586439a1a15427a135
Will <willdwelle@frontier.com>
2021-12-05 12:40:49 -0800

    fix

--- TestBank.java
+++ TestBank.java
@@ -1,6 +1,7 @@
 public class TestBank {
    public static void main(String[] args) {
        BankAccount acc1 = new BankAccount(100.00, 250.00);
-       acc1.deposit(checking, 20);
+       acc1.deposit("checking", 20);
+       acc1.showTotal();
    } 
 }
